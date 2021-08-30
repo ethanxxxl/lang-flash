ce0b558ef2c2dc0e9a6439e716fe66ac12a935aa
Ethan Smith <ethansmith.dev@gmail.com>
2021-08-30 01:31:14 -0400

    Refactored the state machine
    program no longer crashes when it runs out of cards.
    Now tracks time.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::error::Error;
 use std::env;
+use std::time::{SystemTime, Duration, UNIX_EPOCH};
+use std::io::{Write, stdin, stdout};
 
 use csv;
-use std::io::{Write, stdin, stdout};
 use termion::input::TermRead;
 use termion::event::Key;
 use termion::raw::IntoRawMode;
+use rand::prelude::*;
 
 #[derive(Debug)]
 struct Word {
     level: u32,
+    last_viewed: SystemTime,
     foreign: String,
     native: String,
 }
@@ -24,12 +27,40 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut cards = Vec::new();
     for result in rdr.records() {
         let record = result?;
+        let last_viewed = Duration::from_secs(record[1].parse().unwrap());
+        let last_viewed = SystemTime::UNIX_EPOCH + last_viewed;
         cards.push(Word {
             level: record[0].parse().unwrap(),
-            foreign: record[1].to_string().trim().to_string(),
-            native: record[2].to_string().trim().to_string(),
+            last_viewed,
+            foreign: record[2].to_string().trim().to_string(),
+            native: record[3].to_string().trim().to_string(),
         });
     }
+
+    let mut due_cards = cards.iter_mut().filter(|card| {
+        let time_elapsed = SystemTime::now().duration_since(card.last_viewed).unwrap();
+        match card.level {
+            0 => time_elapsed >= Duration::from_secs(60),
+            1 => time_elapsed >= Duration::from_secs(60 * 10),
+            2 => time_elapsed >= Duration::from_secs(60 * 60 * 12),
+            3 => time_elapsed >= Duration::from_secs(60 * 60 * 24),
+            4 => time_elapsed >= Duration::from_secs(60 * 60 * 24 * 5),
+            5 => time_elapsed >= Duration::from_secs(60 * 60 * 24 * 20),
+            _ => time_elapsed >= Duration::from_secs(60 * 60 * 24 * 60),
+        }
+    }).collect::<Vec<&mut Word>>();
+
+    if due_cards.is_empty() {
+        println!("no cards due!");
+        return Ok(());
+    }
+
+    let mut rng = thread_rng();
+    due_cards.shuffle(&mut rng);
+    let mut due_cards = due_cards.iter_mut();
+
+    // we can unwrap here because there will be at least 1 card
+    let mut current_card = due_cards.next().unwrap();
 
     let stdout = stdout();
     let mut stdout = stdout.lock().into_raw_mode()?;
@@ -44,17 +75,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     stdout.flush()?;
 
-    let mut card = cards.iter();
-    let mut is_prompting = true;
+    enum State {
+        Prompt,
+        ShowAnswer,
+        SaveExit,
+    }
 
-    let mut current_word = card.next().unwrap();
+    let mut state = State::Prompt;
 
     for c in stdin.keys() {
         write!(stdout,
                "{}",
                termion::clear::All
         )?;
-
         let c = c?;
 
         // common keys
@@ -64,53 +97,74 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         // Manage state
-        if is_prompting {
-            match c {
-                Key::Char(' ') => is_prompting = false,
-                _ => {},
-            }
-        } else {
-            match c {
-                Key::Char(' ') | Key::Char('1') => {
-                    // increment and save file
-                    current_word = card.next().unwrap();
-                    is_prompting = true;
-                },
-                Key::Char('2') => {
-                    current_word = card.next().unwrap();
-                    is_prompting = true;
-                    // reset to 0
+        match state {
+            State::Prompt => {
+                match c {
+                    Key::Char(' ') => state = State::ShowAnswer,
+                    _ => {},
                 }
-                _ => {},
-            }
+            },
+            State::ShowAnswer => {
+                match c {
+                    Key::Char(' ') | Key::Char('1') => {
+                        current_card.level += 1;
+                        if let Some(next) = due_cards.next() {
+                            current_card = next;
+                            state = State::Prompt;
+                        } else {
+                            state = State::SaveExit;
+                        }
+                    },
+                    Key::Char('2') => {
+                        current_card.level = 0;
+                        if let Some(next) = due_cards.next() {
+                            current_card = next;
+                            state = State::Prompt;
+                        } else {
+                            state = State::SaveExit;
+                        }
+                    }
+                    _ => {},
+                }
+            },
+            State::SaveExit => {
+                return Ok(());
+                // TODO implement save and exit
+            },
         }
 
         // draw current screen (tied to state)
-        if is_prompting {
-            write!(stdout,
-                    "{}{}",
-                    termion::cursor::Goto(1,1),
-                    current_word.foreign,
-            )?;
-            write!(stdout,
-                    "{}press space to flip",
-                    termion::cursor::Goto(1,2),
-            )?;
-        } else {
-            write!(stdout,
-                    "{}{} -> {}",
-                    termion::cursor::Goto(1,1),
-                    current_word.foreign,
-                    current_word.native,
-            )?;
-            write!(stdout,
-                    "{}{}(1) Correct{}  {}(2) Incorrect{}",
-                    termion::cursor::Goto(1,2),
-                    termion::style::Underline,
-                    termion::style::Reset,
-                    termion::style::Underline,
-                    termion::style::Reset,
-            )?;
+        match state {
+            State::Prompt => {
+                write!(stdout,
+                        "{}{}",
+                        termion::cursor::Goto(1,1),
+                        current_card.foreign,
+                )?;
+                write!(stdout,
+                        "{}press space to flip",
+                        termion::cursor::Goto(1,2),
+                )?;
+            },
+            State::ShowAnswer => {
+                write!(stdout,
+                        "{}{} -> {}",
+                        termion::cursor::Goto(1,1),
+                        current_card.foreign,
+                        current_card.native,
+                )?;
+                write!(stdout,
+                        "{}{}(1) Correct{}  {}(2) Incorrect{}",
+                        termion::cursor::Goto(1,2),
+                        termion::style::Underline,
+                        termion::style::Reset,
+                        termion::style::Underline,
+                        termion::style::Reset,
+                )?;
+            },
+            State::SaveExit => {
+
+            }
         }
 
         stdout.flush()?;
