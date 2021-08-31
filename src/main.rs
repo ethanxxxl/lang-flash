7ad5f7fb92b04c63f355261d5c274b3dcc7f44df
Ethan Smith <ethansmith.dev@gmail.com>
2021-08-30 23:13:56 -0400

    stores level and last viewed time in json file
    csv file now only holds the necessary data, as originally envisioned.
    a json file is used to cache the card data / state between runs. You can
    alter the csv file and those changes will be propogated to the json file
    seamlessly during the next run of the program.

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,86 @@ use std::error::Error;
 use std::env;
 use std::time::{SystemTime, Duration, UNIX_EPOCH};
 use std::io::{Write, stdin, stdout};
+use std::fs;
+use std::path::{Path, PathBuf};
 
 use csv;
+use serde_json::{self, Value};
+use serde::{Deserialize, Serialize};
 use termion::input::TermRead;
 use termion::event::Key;
 use termion::raw::IntoRawMode;
 use rand::prelude::*;
+use std::collections::HashMap;
 
-#[derive(Debug)]
-struct Word {
-    level: u32,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct CardData {
+    answer: String,
+    level: u64,
     last_viewed: SystemTime,
-    foreign: String,
-    native: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let file_path = args.get(1).expect("error! expected 1 argument, got 0");
+    let mut file_path = PathBuf::new();
 
-    let mut rdr = csv::Reader::from_path(file_path)?;
+    file_path.push(Path::new(args.get(1).expect("error! expected 1 argument, got 0")));
+    let mut meta_data_path = file_path.clone();
+    meta_data_path.set_file_name({
+        let mut name = file_path
+            .file_name()
+            .expect("error! You gave a stupid input, you got a stupid output")
+            .to_os_string()
+            .into_string()
+            .expect("error! illegal file name.");
 
-    let mut cards = Vec::new();
-    for result in rdr.records() {
-        let record = result?;
-        let last_viewed = Duration::from_secs(record[1].parse().unwrap());
-        let last_viewed = SystemTime::UNIX_EPOCH + last_viewed;
-        cards.push(Word {
-            level: record[0].parse().unwrap(),
-            last_viewed,
-            foreign: record[2].to_string().trim().to_string(),
-            native: record[3].to_string().trim().to_string(),
-        });
+        name.insert(0, '.');
+        name
+    });
+    meta_data_path.set_extension("json");
+
+    let mut cached_cards: HashMap<String, CardData> = HashMap::new();
+    // if a data file exists, populate the cards hash.
+    if let Ok(data) = fs::read_to_string(meta_data_path.clone()) {
+        cached_cards = serde_json::from_str(&data.as_str()).expect("json error");
     }
 
-    let mut due_cards = cards.iter_mut().filter(|card| {
+    let mut cards: HashMap<String, CardData> = HashMap::new();
+    let mut csv_reader = csv::Reader::from_path(file_path)?;
+    for result in csv_reader.records() {
+        let record = result?;
+
+        // update json file with new / removed cards
+        let key = record[0].to_string();
+        if let Some(card_data) = cached_cards.remove(&key) {
+            cards.insert(key, card_data);
+        } else {
+            cards.insert(
+                key,
+                CardData {
+                    answer: record[1].to_string(),
+                    level: 0,
+                    last_viewed: UNIX_EPOCH,
+                });
+        }
+    }
+
+    // filter out cards which aren't due
+    let mut due_cards = cards.iter_mut().filter(|(_, card)| {
         let time_elapsed = SystemTime::now().duration_since(card.last_viewed).unwrap();
+        println!("{}: {:?}", card.level, time_elapsed);
         match card.level {
-            0 => time_elapsed >= Duration::from_secs(60),
-            1 => time_elapsed >= Duration::from_secs(60 * 10),
-            2 => time_elapsed >= Duration::from_secs(60 * 60 * 12),
-            3 => time_elapsed >= Duration::from_secs(60 * 60 * 24),
-            4 => time_elapsed >= Duration::from_secs(60 * 60 * 24 * 5),
-            5 => time_elapsed >= Duration::from_secs(60 * 60 * 24 * 20),
+            0 => time_elapsed >= Duration::from_secs(0),
+            1 => time_elapsed >= Duration::from_secs(60),
+            2 => time_elapsed >= Duration::from_secs(60 * 10),
+            3 => time_elapsed >= Duration::from_secs(60 * 60 * 12),
+            4 => time_elapsed >= Duration::from_secs(60 * 60 * 24),
+            5 => time_elapsed >= Duration::from_secs(60 * 60 * 24 * 5),
+            6 => time_elapsed >= Duration::from_secs(60 * 60 * 24 * 20),
             _ => time_elapsed >= Duration::from_secs(60 * 60 * 24 * 60),
         }
-    }).collect::<Vec<&mut Word>>();
+    }).collect::<Vec<(&String, &mut CardData)>>();
 
     if due_cards.is_empty() {
         println!("no cards due!");
@@ -107,7 +140,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             State::ShowAnswer => {
                 match c {
                     Key::Char(' ') | Key::Char('1') => {
-                        current_card.level += 1;
+                        current_card.1.level += 1;
+                        current_card.1.last_viewed = SystemTime::now();
                         if let Some(next) = due_cards.next() {
                             current_card = next;
                             state = State::Prompt;
@@ -116,7 +150,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                         }
                     },
                     Key::Char('2') => {
-                        current_card.level = 0;
+                        current_card.1.level = 0;
+                        current_card.1.last_viewed = SystemTime::now();
                         if let Some(next) = due_cards.next() {
                             current_card = next;
                             state = State::Prompt;
@@ -128,18 +163,20 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             },
             State::SaveExit => {
+                let json = serde_json::to_string(&cards.clone()).expect("error! couldn't save json!");
+                fs::write(meta_data_path.clone(), json);
                 return Ok(());
-                // TODO implement save and exit
             },
         }
 
         // draw current screen (tied to state)
+        let (question, answer) = (current_card.0, &current_card.1.answer);
         match state {
             State::Prompt => {
                 write!(stdout,
                         "{}{}",
                         termion::cursor::Goto(1,1),
-                        current_card.foreign,
+                        question,
                 )?;
                 write!(stdout,
                         "{}press space to flip",
@@ -150,8 +187,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                 write!(stdout,
                         "{}{} -> {}",
                         termion::cursor::Goto(1,1),
-                        current_card.foreign,
-                        current_card.native,
+                        question,
+                        answer,
                 )?;
                 write!(stdout,
                         "{}{}(1) Correct{}  {}(2) Incorrect{}",
